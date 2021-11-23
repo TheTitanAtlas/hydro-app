62f145bf145bd69b50795f881b3901860d0d166b
TheTitanAtlas <atlas.peter.olsson@gmail.com>
2021-11-23 18:38:57 +0100

    we remade the base to be tab-based instead

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'hydro-app',
+  appName: 'TheTinyHydroGarden',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Plants', url: '/folder/Plants', icon: 'flower' },
-    { title: 'Notes', url: '/folder/Notes', icon: 'create' },
-    { title: 'Chat', url: '/folder/Chat', icon: 'chatbubbles' },
-    { title: 'Account', url: '/folder/Account', icon: 'person' },
-  ];
-  public labels = ['Seed',];
   constructor() {}
 }
